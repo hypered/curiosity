6bd121eb116d1c791961003cb8aca1613ed9f59a
Vo Minh Thu <thu@hypered.io>
2023-07-17 16:55:27 +0200

    Update the landing page.
    This move the landing page definition from the smart-design-hs
    repository to this repository, and update the text.

--- src/Curiosity/Html/LandingPage.hs
+++ src/Curiosity/Html/LandingPage.hs
@@ -1,14 +1,23 @@
+{-# LANGUAGE DataKinds #-}
 {- |
 Module: Curiosity.Html.LandingPage
-Description: A landing page (when the user is not logge in) for Curiosity.
+Description: A landing page (when the user is not logged in) for Curiosity.
 -}
 module Curiosity.Html.LandingPage
   ( LandingPage(..)
   ) where
 
-import qualified Smart.Html.Pages.LandingPage  as Pages
+import           Smart.Html.Alert
+import           Smart.Html.Button
+import           Smart.Html.Dsl                 ( HtmlCanvas )
+import qualified Smart.Html.Dsl                as Dsl
+import qualified Smart.Html.Misc               as Misc
+import           Smart.Html.Navbar
+import           Smart.Html.Shared.Html.Icons
 import qualified Smart.Html.Render             as Render
 import qualified Text.Blaze.Html5              as H
+import           Text.Blaze.Html5               ( Html, (!) )
+import qualified Text.Blaze.Html5.Attributes   as A
 
 
 --------------------------------------------------------------------------------
@@ -16,4 +25,47 @@ data LandingPage = LandingPage
 
 instance H.ToMarkup LandingPage where
   toMarkup LandingPage = do
-    Render.renderCanvas Pages.landingPage
+    Render.renderCanvas landingPage
+
+
+--------------------------------------------------------------------------------
+landingPage :: HtmlCanvas
+landingPage = Dsl.SingletonCanvas $ do
+  H.toMarkup navigation
+  landing
+  Misc.landingFooter "https://github.com/hypered/curiosity"
+
+navigation :: NavbarWebsite
+navigation = NavbarWebsite
+  [Entry "Documentation" (Link "/documentation"), Entry "Login" (Link "/login"), Entry "Sign up" (Link "/signup")]
+
+landing :: Html
+landing = H.main ! A.class_ "o-container o-container--flex" $ do
+  Misc.landingHero "Curiosity, a prototype system"
+    $ do
+        H.p $ do
+          "This site is a running instance of Curiosity. \
+          \Curiosity is an always \
+          \work-in-progress system to think, discuss, and communicate \
+          \development practices and system design."
+        H.toMarkup $ Alert
+          AlertWarning
+          iconWarning
+          "This site is up for demonstration purpose only. Data are public \
+          \(including passwords) \
+          \and frequently permanently erased. Please use this site only \
+          \if you understand what this means."
+          NoButton
+  H.div ! A.class_ "o-grid" $ do
+    Misc.landingPanel "Open source" $ H.p $ do
+      "This project is open source "
+      "and available "
+      H.a ! A.href "https://github.com/hypered/curiosity" $ "on GitHub"
+      "."
+    Misc.landingPanel "Experimental" $ H.p $ do
+      "Curiosity is being developed to inform further developments, and "
+      "is not intended to be a production system."
+
+-- TODO Move this little helper definitions to Icons.
+iconWarning :: Maybe (OSvgIconDiv "warning")
+iconWarning = Just $ OSvgIconDiv @"warning" svgIconWarning
